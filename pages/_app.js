f831f3d5a9c69c2c3bc1ba93583bf1135ec26d2e
Skylurk <41697386+skylurk@users.noreply.github.com>
2023-02-14 11:03:22 +0300

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
+import Script from 'next/script'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Script strategy='afterInteractive' src="https://www.googletagmanager.com/gtag/js?id=G-LY2BTLKQZS" />
+    <Script id='google-analytics' strategy='afterInteractive'>
+      {
+        `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-LY2BTLKQZS', {
+          page_path: window.location.pathname,
+        });`
+      }
+    </Script>
+    <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
